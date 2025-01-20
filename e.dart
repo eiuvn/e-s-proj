0030f735a9d07b29a6a91fe48ba00c535c440697
eiuvn <weowos@gmail.com>
2025-01-20 08:25:42 +0300

    Add files via upload

---
+++ e.dart
@@ -0,0 +1,11 @@
+import 'package:e/e.dart' as e;
+
+void main(List<String> arguments) {
+  int age = 20;
+  if(age>=13 && age<=19)
+     print("teenager");
+     else
+     print("not teenager");
+
+}
+  
